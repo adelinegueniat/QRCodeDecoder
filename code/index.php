7c39f239c78b7898fb214ad4a5678ac296638eb9
adelinegueniat <gueniat.adeline@gmail.com>
2019-05-01 10:23:57 +0200

    test qrreader with static file

--- code/index.php
+++ code/index.php
@@ -1,3 +1,7 @@
 <?php
-phpinfo();
+require __DIR__ . "/vendor/autoload.php";
+
+$qrcode = new Zxing\QrReader('qrcodes/qr55.png');
+$text = $qrcode->text(); //return decoded text from QR Code
+echo $text;
 ?>
