4c162f6f2be24fe89bbe9ea08f0f3a8098fd0978
adelinegueniat <gueniat.adeline@gmail.com>
2019-05-01 11:12:13 +0200

    add decode part to upload confirmation

--- code/upload.php
+++ code/upload.php
@@ -1,4 +1,6 @@
 <?php
+require __DIR__ . "/vendor/autoload.php";
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -8,6 +10,7 @@ if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
+        echo "<br>";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -22,6 +25,11 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "<br>";
+        $qrcode = new Zxing\QrReader($target_file);
+        $text = $qrcode->text(); //return decoded text from QR Code
+        echo $text;
+
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
